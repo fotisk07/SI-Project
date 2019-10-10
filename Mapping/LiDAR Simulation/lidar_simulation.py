dbac4a2541782fef061b52d6798a0a660ae50712
Fotis Kapotos <fotiskapotos@gmail.com>
2019-10-10 21:34:22 +0200

    Bug Fix

--- Mapping/LiDAR Simulation/lidar_simulation.py
+++ Mapping/LiDAR Simulation/lidar_simulation.py
@@ -10,11 +10,12 @@ carte[9][:]=1
 carte[:,9]=1
 angle_step = 1
 ray_step = 1
-current_ray = 1 
+
 data = []
 
 print(carte)
 for i in range(int(360/angle_step)):
+    current_ray = 1 
     while True:
         x_current = int(m.cos(m.radians(i))*ray_step*current_ray)
         y_current = int(m.sin(m.radians(i))*ray_step*current_ray)
@@ -24,7 +25,7 @@ for i in range(int(360/angle_step)):
             if carte[x_current+pos[0]][y_current+pos[1]] == 1:
                 data.append((current_ray , i))
                 break
-            current_ray += 1
+            current_ray += 0.5
         except:
                 data.append((current_ray , i))
                 break
