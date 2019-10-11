a802af51c605e278b36e27d6235f46802a926168
Fotis Kapotos <fotiskapotos@gmail.com>
2019-10-11 21:14:55 +0200

    Add noise function

--- Mapping/LiDAR Simulation/lidar_simulation.py
+++ Mapping/LiDAR Simulation/lidar_simulation.py
@@ -1,31 +1,38 @@
 import numpy as np
 import math as m
 dim = (10,10)
-
 carte = np.zeros(dim)
 pos = np.array([5,5])
+carte[pos[0]][pos[1]] = 0.5
 carte[0][:]=1
 carte[:,0]=1
 carte[9][:]=1
 carte[:,9]=1
 angle_step = 1
 ray_step = 1
-
+mu = 0
+std = 0.01
 data = []
+
+def noise(clean_data):
+    data = np.array(clean_data)
+    noise = np.random.normal(mu,std,[int(360/angle_step),2])
+    return noise+clean_data
+    
 
 print(carte)
 for i in range(int(360/angle_step)):
     current_ray = 1 
     while True:
-        x_current = int(m.cos(m.radians(i))*ray_step*current_ray)
-        y_current = int(m.sin(m.radians(i))*ray_step*current_ray)
+        x_current = int(m.cos(m.radians(i))*current_ray)
+        y_current = int(m.sin(m.radians(i))*current_ray)
         #print(x_current+pos[0],y_current+pos[1])
         #print(carte[x_current+pos[0]][y_current+pos[1]])
         try:
             if carte[x_current+pos[0]][y_current+pos[1]] == 1:
                 data.append((current_ray , i))
                 break
-            current_ray += 0.5
+            current_ray += ray_step
         except:
                 data.append((current_ray , i))
                 break
