80afbaae3c2e4a9d9864c54955d941abcb9ca46c
Mateo RIVERA CRUZ <mateorivera@MacBook-Pro-de-Mateo.local>
2019-10-26 15:15:10 +0200

    Based on testing, this fixes the bound error

--- Mapping/Map.py
+++ Mapping/Map.py
@@ -5,7 +5,7 @@ from LiSim import sim
 from bresenham import bresenham
 
 def rotation(theta):
-    return np.array([[np.cos(m.radians(theta)),np.sin(m.radians(theta))],[-np.sin(m.radians(theta)),np.cos(m.radians(theta))]])
+    return np.array([[np.cos(m.radians(theta)),-np.sin(m.radians(theta))],[np.sin(m.radians(theta)),np.cos(m.radians(theta))]])
 
 dim = (10,10)
 pos = np.array([5,5])
@@ -17,27 +17,15 @@ lidar = sim.Lidar(dim=dim,pos=pos)
 measure = lidar.simulate(show=True)
 
 for i in range(360):
-    try:
-        # QUESTION: Will raise an exception sometimes, why ?
-        occ = (np.matmul( rotation(measure[i][1]), [measure[i][0],0] ) + pos).astype(int)
-        
-        carte[occ[0]][occ[1]] += logodd_occ
-        
-        #Draw a line Lidar <-> Point, and register all points on the line as unoccupied (the lidar would have detected if not)
-        points = np.array(list(bresenham(occ[0],occ[1],pos[0],pos[1])))
-        for x,y in points:
-            if x!=occ[0] and y!=occ[1]:
-                carte[x][y] -= logodd_free
 
+    occ = (np.matmul( rotation(measure[i][1]), [measure[i][0],0] ) + pos).astype(int)
 
-    except:
-        # QUESTION: Why is the measurement shifted by (-1,-1) ?
-            carte[occ[0]-1][occ[1]-1] += logodd_occ
-            points = np.array(list(bresenham(occ[0]-1,occ[1]-1,pos[0],pos[1])))
-            for x,y in points:
-                if x!=occ[0]-1 and y!=occ[1]-1:
-                    carte[x][y] -= logodd_free
+    carte[occ[0]][occ[1]] += logodd_occ
 
-
+    #Draw a line Lidar <-> Point, and register all points on the line as unoccupied (the lidar would have detected if not)
+    points = np.array(list(bresenham(occ[0],occ[1],pos[0],pos[1])))
+    for x,y in points:
+        if x!=occ[0] and y!=occ[1]:
+            carte[x][y] -= logodd_free
 
 print(carte)
