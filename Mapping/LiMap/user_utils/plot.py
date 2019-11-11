be460e3dbb77e44f66b858b6c239846c8c8b1e60
Fotis Kapotos <fotiskapotos@gmail.com>
2019-11-11 15:40:06 +0100

    Scalling the animation

--- Mapping/LiMap/user_utils/plot.py
+++ Mapping/LiMap/user_utils/plot.py
@@ -49,11 +49,13 @@ def plotData(data, name, show=False, printa=False, save=True,path=""):
 def animate(image,center_coordinates):
     radius = 20
     color = (255, 255, 255)
-    thickness = 2
+    thickness = 1
     
     image = cv2.applyColorMap(image.astype(np.uint8), cv2.COLORMAP_JET)
     #cv2.adaptiveThreshold(image, 255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,2,0)
     image = cv2.circle(image, center_coordinates, 1, color, thickness)
+    image = cv2.resize(image,(500,500))
+    
     
     cv2.imshow("produced_carte", image)
     
